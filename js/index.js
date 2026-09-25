e219b8ca833dad64a44664c0b3082fdfe378b516
edgardo9 <e.llanos1989@gmail.com>
2026-09-25 17:08:34 -0300

    Se corrigen comentarios segun lo sugerido en fem

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ async function getDataJson() {
 
 function addNodeCard(data, lapsus="Weekly") {
     cardsDetailCont.textContent = "";
-    for(cardData of data) {
+    for(let cardData of data) {
         createNodes(cardData, lapsus)
     }
 }
